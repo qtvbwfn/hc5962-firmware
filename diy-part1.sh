4ef953e79fbc2f2da6fd0f52372d0527896ca446
China.Roger <qtvbwfn@vip.qq.com>
2021-10-18 15:42:06 +0800

    Update diy-part1.sh
    增加应用协议过滤插件

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default 
+echo 'src-git oaf https://github.com/destan19/OpenAppFilter' >>feeds.conf.default 
