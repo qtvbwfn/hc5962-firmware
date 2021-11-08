32e3072f43e27ef7c61de22ec1e322b648e1fa40
China.Roger <qtvbwfn@vip.qq.com>
2021-11-08 13:59:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,11 +14,11 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default 
-echo 'src-git oaf https://github.com/destan19/OpenAppFilter' >>feeds.conf.default 
-echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default 
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default 
+#echo 'src-git oaf https://github.com/destan19/OpenAppFilter' >>feeds.conf.default 
+#echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default 
 #echo 'src-git jerrykuku https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default 
 #echo 'src-git alexzhuo https://github.com/AlexZhuo/luci-app-bandwidthd' >>feeds.conf.default 
 
