52a18b669aa7147505b96a9a3f039be7f8bb85db
China.Roger <qtvbwfn@vip.qq.com>
2022-11-22 12:46:32 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,3 +22,7 @@
 #echo 'src-git jerrykuku https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default 
 #echo 'src-git alexzhuo https://github.com/AlexZhuo/luci-app-bandwidthd' >>feeds.conf.default 
 
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git oaf https://github.com/destan19/OpenAppFilter ' >>feeds.conf.default
